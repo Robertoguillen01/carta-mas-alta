ac2cde0e967f9450aa447b5ee811c5922190a703
Robertoguillen01 <136328997+Robertoguillen01@users.noreply.github.com>
2023-06-14 09:43:35 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+a = 0
+b = 0
+while a < 5 and b < 5:
+  num1 = int(input('Introduce un numero: '))
+  num2 = int(input('Introduce otro numero: ')) 
+if num1 > num2:
+   (a + 1)
+   a = a + 1
+else:
+   (b + 1)
+   b = b + 1
+  if a >= 5:
+    print('Jugador 1 gana')
+    if b >= 5:
+     print('Jugador 2 gana')
